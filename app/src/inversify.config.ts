9f95bbeaf0771cda15cb4cd68f857c1938d57845
Shahar Dahan <dahshahar@gmail.com>
2021-01-22 20:11:05 -0500

    Fix the build directory to not include tests

--- app/src/inversify.config.ts
+++ app/src/inversify.config.ts
@@ -8,12 +8,19 @@ import {CompoundMessageHandler} from './messages/compound-message-handler';
 import {QuoteBotMessageHandler} from './messages/quote-bot-message-handler';
 import {Client} from 'discord.js';
 
+function stringOrThrow(check: string | undefined) : string {
+  if (check !== null && check !== undefined) {
+    return check;
+  }
+  throw `Expected ${check} to be a string, was not`
+}
+
 let container = new Container();
 
 container.bind<Bot>(TYPES.Bot).to(Bot).inSingletonScope();
 container.bind<Client>(TYPES.Client).toConstantValue(new Client());
-container.bind<string>(TYPES.Token).toConstantValue(process.env.TOKEN);
-container.bind<string>(TYPES.Qualifier).toConstantValue(process.env.QUALIFIER);
+container.bind<string>(TYPES.Token).toConstantValue(stringOrThrow(process.env.TOKEN));
+container.bind<string>(TYPES.Qualifier).toConstantValue(stringOrThrow(process.env.QUALIFIER));
 
 const messageHandlers: MessageHandler[] = [
   new EchoHandler(),
