74a955df4104baa9ed7d47949223cb9fb7d3cdb7
Shahar Dahan <dahshahar@gmail.com>
2021-02-02 20:47:18 -0500

    Add a bucket to persist quotes in

--- cdk/lib/quote-bot-stack.ts
+++ cdk/lib/quote-bot-stack.ts
@@ -1,9 +1,15 @@
 import * as cdk from '@aws-cdk/core';
+import { Bucket, BlockPublicAccess } from '@aws-cdk/aws-s3';
 
 export class QuoteBotStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const quoteBotBucket = new Bucket(this, 'QuoteBucketBackup', {
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+    });
+
+    new cdk.CfnOutput(this, 'S3Bucket', { value: quoteBotBucket.bucketName });
   }
 }
