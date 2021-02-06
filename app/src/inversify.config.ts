6a7ad043cc965072e49543085960828fe31b33dd
Shahar Dahan <dahshahar@gmail.com>
2021-02-05 21:38:48 -0500

    Flush to S3 to persist in-memory-manager

--- app/src/inversify.config.ts
+++ app/src/inversify.config.ts
@@ -1,9 +1,8 @@
-import { readFileSync, writeFileSync } from 'fs';
+import { Client } from 'discord.js';
 import 'reflect-metadata';
 import { Container } from 'inversify';
 import { TYPES } from './types';
 import { Bot } from './bot';
-import { Quote } from './quotes/quote';
 import { MessageHandler } from './messages/message-handler';
 import { EchoHandler } from './messages/echo-handler';
 import { AddQuoteHandler } from './messages/add-quote-handler';
@@ -17,39 +16,14 @@ import { BlameQuoteFormatter } from './quotes/blame-quote-formatter';
 import { QuoteFormatter } from './quotes/quote-formatter';
 import { QuoteManager } from './quotes/quote-manager';
 import { InMemoryQuoteManager } from './quotes/in-memory-quote-manager';
-import { Client } from 'discord.js';
 
-const QUOTE_MAPPING_PATH = './quoteMapping.json';
+const QUOTE_MAPPING_PATH = 'quoteMapping.json';
 
 function stringOrThrow(check: string | undefined, errorMessage: string): string {
   if (check !== null && check !== undefined) {
     return check;
   }
   throw new Error(errorMessage);
-}
-
-function flushJson(path: string, mapping: Map<any, any>) {
-  try {
-    writeFileSync(path, JSON.stringify(Object.fromEntries(mapping)));
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-function loadMappingFromFile(path: string): Map<number, Quote> {
-  try {
-    const jsonMap = new Map<string, Quote>(Object.entries(JSON.parse(readFileSync(path, 'utf8'))));
-    const mapping = new Map<number, Quote>();
-    jsonMap.forEach((value, key) => {
-      mapping.set(parseInt(key), value);
-    });
-    console.log('Loaded mapping from file!');
-    console.log(mapping);
-    return mapping;
-  } catch (err) {
-    console.error('Could not find file');
-    return new Map<number, Quote>();
-  }
 }
 
 const container = new Container();
@@ -64,10 +38,13 @@ container
 container.bind<QuoteFormatter>(TYPES.BasicQuoteFormatter).to(BasicQuoteFormatter).inSingletonScope();
 container.bind<QuoteFormatter>(TYPES.BlameQuoteFormatter).to(BlameQuoteFormatter).inSingletonScope();
 
-const quoteMapping: Map<number, Quote> = loadMappingFromFile(QUOTE_MAPPING_PATH);
-container.bind<Map<number, Quote>>(TYPES.QuoteMapping).toConstantValue(quoteMapping);
+const quoteManager = new InMemoryQuoteManager();
+quoteManager.getQuotesFromS3(QUOTE_MAPPING_PATH).catch(() => {
+  console.error('Failed to get quotes from S3');
+});
+quoteManager.addExitHook(QUOTE_MAPPING_PATH);
 
-container.bind<QuoteManager>(TYPES.QuoteManager).to(InMemoryQuoteManager).inSingletonScope();
+container.bind<QuoteManager>(TYPES.QuoteManager).toConstantValue(quoteManager);
 
 const messageHandlers: MessageHandler[] = [
   new EchoHandler(),
@@ -80,30 +57,5 @@ const messageHandlers: MessageHandler[] = [
 container.bind<MessageHandler[]>(TYPES.MessageHandlers).toConstantValue(messageHandlers);
 
 container.bind<CompoundMessageHandler>(TYPES.MessageHandler).to(QuoteBotMessageHandler).inSingletonScope();
-
-[
-  'beforeExit',
-  'uncaughtException',
-  'unhandledRejection',
-  'SIGHUP',
-  'SIGINT',
-  'SIGQUIT',
-  'SIGILL',
-  'SIGTRAP',
-  'SIGABRT',
-  'SIGBUS',
-  'SIGFPE',
-  'SIGUSR1',
-  'SIGSEGV',
-  'SIGUSR2',
-  'SIGTERM',
-].forEach((event) =>
-  process.on(event, (evt) => {
-    flushJson(QUOTE_MAPPING_PATH, quoteMapping);
-    console.log('Flushed Json to file');
-    console.log(readFileSync(QUOTE_MAPPING_PATH, 'utf8'));
-    process.exit();
-  })
-);
 
 export default container;
--- app/src/quotes/in-memory-quote-manager.ts
+++ app/src/quotes/in-memory-quote-manager.ts
@@ -1,3 +1,4 @@
+import { S3Client, GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3';
 import { QuoteManager } from './quote-manager';
 import { Quote } from './quote';
 import { TYPES } from '../types';
@@ -6,6 +7,7 @@ import { inject, injectable } from 'inversify';
 @injectable()
 export class InMemoryQuoteManager implements QuoteManager {
   private quotes: Map<number, Quote>;
+  private s3Client: S3Client;
 
   constructor(@inject(TYPES.QuoteMapping) quotes?: Map<number, Quote>) {
     if (quotes) {
@@ -13,6 +15,7 @@ export class InMemoryQuoteManager implements QuoteManager {
     } else {
       this.quotes = new Map<number, Quote>();
     }
+    this.s3Client = new S3Client({});
   }
 
   get(): Quote | undefined {
@@ -28,8 +31,6 @@ export class InMemoryQuoteManager implements QuoteManager {
   getBySearch(search: string): Map<number, Quote> {
     const quotesMap: Map<number, Quote> = new Map<number, Quote>();
     this.quotes.forEach((value, key) => {
-      console.log(value.quote);
-      console.log(search);
       if (
         value.quote.toLowerCase().includes(search.toLowerCase()) ||
         value.author.toLowerCase() === search.toLowerCase()
@@ -43,5 +44,74 @@ export class InMemoryQuoteManager implements QuoteManager {
   add(quote: Quote): boolean {
     this.quotes.set(this.quotes.size + 1, quote);
     return true;
+  }
+
+  async getQuotesFromS3(path: string): Promise<void> {
+    try {
+      const getObjectCommand = new GetObjectCommand({
+        Bucket: process.env.BUCKET_NAME,
+        Key: path,
+      });
+      const getObjectResult = await this.s3Client.send(getObjectCommand);
+      const chunks = [];
+      for await (const chunk of getObjectResult.Body) {
+        chunks.push(chunk);
+      }
+      const chunkString = Buffer.concat(chunks).toString('utf-8');
+      const jsonMap = new Map<string, Quote>(Object.entries(JSON.parse(chunkString)));
+      const mapping = new Map<number, Quote>();
+      jsonMap.forEach((value, key) => {
+        mapping.set(parseInt(key), value);
+      });
+      console.log('Loaded mapping from file!');
+      console.log(mapping);
+      this.quotes = mapping;
+    } catch (err) {
+      console.error(err);
+      this.quotes = new Map<number, Quote>();
+    }
+  }
+
+  async flushJson(path: string): Promise<void> {
+    try {
+      const putObjectCommand = new PutObjectCommand({
+        Bucket: process.env.BUCKET_NAME,
+        Key: path,
+        Body: JSON.stringify(Object.fromEntries(this.quotes)),
+      });
+      await this.s3Client.send(putObjectCommand);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  addExitHook(path: string): void {
+    [
+      'beforeExit',
+      'uncaughtException',
+      'unhandledRejection',
+      'SIGHUP',
+      'SIGINT',
+      'SIGQUIT',
+      'SIGILL',
+      'SIGTRAP',
+      'SIGABRT',
+      'SIGBUS',
+      'SIGFPE',
+      'SIGUSR1',
+      'SIGSEGV',
+      'SIGUSR2',
+      'SIGTERM',
+    ].forEach((event) =>
+      process.on(event, () => {
+        (async () => {
+          await this.flushJson(path);
+          console.log('Flushed Json to S3');
+          process.exit();
+        })().catch(() => {
+          console.log('Failed to flush');
+        });
+      })
+    );
   }
 }
