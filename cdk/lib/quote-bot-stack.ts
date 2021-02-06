446a047e5ecbdd3794ebc24d8294d4d67ca4646a
Shahar Dahan <dahshahar@gmail.com>
2021-02-06 10:32:44 -0500

    Create a DDB table for quote bot

--- cdk/lib/quote-bot-stack.ts
+++ cdk/lib/quote-bot-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import { Bucket, BlockPublicAccess } from '@aws-cdk/aws-s3';
+import { AttributeType, BillingMode, Table } from '@aws-cdk/aws-dynamodb';
 
 export class QuoteBotStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -9,7 +10,16 @@ export class QuoteBotStack extends cdk.Stack {
     const quoteBotBucket = new Bucket(this, 'QuoteBucketBackup', {
       blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
     });
-
     new cdk.CfnOutput(this, 'S3Bucket', { value: quoteBotBucket.bucketName });
+
+    const quoteBotTable = new Table(this, 'Quotes', {
+      partitionKey: {
+        name: 'quoteIndex',
+        type: AttributeType.STRING
+      },
+      billingMode: BillingMode.PROVISIONED,
+      readCapacity: 5,
+      writeCapacity: 5,
+    });
   }
 }
